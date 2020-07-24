dc97cea6bacc38039a748a9822a5597860a63fcb
ThemApples <Jimmyyo12@gmail.com>
2020-07-25 11:35:03 +1200

    Base code completed

--- Bittercode/app/src/main/java/com/example/bittercode/MainActivity.java
+++ Bittercode/app/src/main/java/com/example/bittercode/MainActivity.java
@@ -17,6 +17,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button generate;
     private Button decode;
 
+    private String s_message;
+    private String s_encrypt;
+    private String s_decrypt;
+
+    private String special;
+    private String special2;
     private Crypto cro;
 
     @Override
@@ -25,7 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         config();
         generateCrypto();
-
     }
 
     private void config()
@@ -44,27 +49,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void generateCrypto()
     {
         cro = new BasicCrypto();
-        String m1 = "hello";
-        String temp = new String(cro.encrypt(m1.getBytes()));
-        String dec = new String(cro.decrypt(temp.getBytes()));
+        getMessage();
+        //showMessage("Message: " + s_message +"\nEncrypt:" + s_encrypt +"\nDecrypt: "+ s_decrypt);
+    }
 
-        showMessage("Message: " + m1 +"\nEncrypt:" + temp +"\nDecrypt: "+dec);
+    private void getdecypt()
+    {
+        s_decrypt = encrypt.getText().toString();
+    }
+
+    private void encryptData()
+    {
+        s_encrypt = new String(cro.encrypt(s_message.getBytes()));
+    }
+
+    private void decyptData()
+    {
+        s_decrypt = new String(cro.decrypt(special2.getBytes()));
+    }
+
+    private void getMessage()
+    {
+        s_message = message.getText().toString();
     }
 
     private void showMessage(String m)
     {
         Toast.makeText(getApplicationContext(), m, Toast.LENGTH_SHORT).show();
-
     }
+
+    private void specialEncryption()
+    {
+        special = "";
+        //int i = str.length() - 1; i >= 0; i--
+        for(int i =s_encrypt.length() - 1; i>=0 ;i--){
+            special = special + s_encrypt.charAt(i);
+        }
+
+        showMessage(special);
+    }
+
+    private void specialEncryption2()
+    {
+        special2 = "";
+        //int i = str.length() - 1; i >= 0; i--
+        for(int i =s_encrypt.length() - 1; i>=0 ;i--){
+            special2 = special2 + s_decrypt.charAt(i);
+        }
+        showMessage(special2);
+    }
+
+
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.generate:
                 showMessage("Generate Button");
+                generateCrypto();
+                encryptData();
+                specialEncryption();
+                encrypt.setText(special);
                 break;
             case R.id.decode:
                 showMessage("decode Button");
+                getdecypt();
+                specialEncryption2();
+                decyptData();
+                decrypt.setText(s_decrypt);
                 break;
         }
     }
